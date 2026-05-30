7b8f5a2573e21aa227be4add5f5d0b2ab1582a1e
MucaErind <168709690+MucaErind@users.noreply.github.com>
2026-05-30 22:15:39 +0000

    Inizializzato progetto Angular e configurato ambiente di sviluppo

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'METTI_QUI_URL_PORTA_5000/api',
+  keycloak: {
+    url: 'METTI_QUI_URL_PORTA_8080',
+    realm: 'eventhub',
+    clientId: 'eventhub-frontend'
+  }
+};
